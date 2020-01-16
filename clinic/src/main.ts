3ed91e769fee60c675c83e4f418ca9f1e48ad2c5
kopepan <koko5688287@gmail.com>
2020-01-16 20:36:49 +0700

    add things login page

--- clinic/src/main.ts
+++ clinic/src/main.ts
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/styles/index.scss'
+
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
--- clinic/src/pages/login/LoginPage.vue
+++ clinic/src/pages/login/LoginPage.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <div>{{loginTitle}}</div>
-    <div>{{usernameTitle}}</div>
-    <div>{{passwordTitle}}</div>
+  <div class="login-page">
+    <div class="container">
+      <div>{{loginTitle}}</div>
+      <hr>
+      <div>{{usernameTitle}}</div>
+      <input type="text" class="form-control">
+      <div>{{passwordTitle}}</div>
+      <input type="password" class="form-control">
+      <button class="btn btn-primary mt-3">
+        {{signInBtnText}}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -13,5 +21,15 @@ export default class LoginPage extends Vue {
   loginTitle: string = 'เข้าสู่ระบบ'
   usernameTitle: string = 'User Name:'
   passwordTitle: string = 'Password:'
+  signInBtnText: string = 'Sign In'
 }
 </script>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
