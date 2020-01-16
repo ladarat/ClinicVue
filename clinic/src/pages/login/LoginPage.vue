ec559e1695fe7c8f306fb7861e8ad4f2593f9a23
kopepan <koko5688287@gmail.com>
2020-01-16 20:16:13 +0700

    add loginPage and import bootstrap

---
+++ clinic/src/pages/login/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{loginTitle}}</div>
+    <div>{{usernameTitle}}</div>
+    <div>{{passwordTitle}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default class LoginPage extends Vue {
+  loginTitle: string = 'เข้าสู่ระบบ'
+  usernameTitle: string = 'User Name:'
+  passwordTitle: string = 'Password:'
+}
+</script>
