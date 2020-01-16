3d4329d67df4a7a80aee533b3f0c12d4f05e58bc
kopepan <koko5688287@gmail.com>
2020-01-16 20:18:40 +0700

    add loginPage to router

--- clinic/src/router/index.ts
+++ clinic/src/router/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import LoginPage from '@/pages/login/LoginPage.vue'
 
 Vue.use(VueRouter)
 
@@ -11,7 +12,7 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: 
+    component: LoginPage
   },
 ]
 
