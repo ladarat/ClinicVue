de40402a9f2d049b9805742cdeccc64a158d9f1c
Suphakrit Nakhabat <suphakrit@odds.team>
2020-01-28 21:40:42 +0700

    Create create patient page

---
+++ clinic/src/pages/patient/create/CreatePatientPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Create Patient
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+
+@Component
+export default class CreatePatientPage extends Vue {
+  
+}
+</script>
