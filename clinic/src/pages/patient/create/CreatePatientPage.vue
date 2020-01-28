e55e7e9559b0370f43f4f765631ff1ca300aadd1
Suphakrit Nakhabat <suphakrit@odds.team>
2020-01-28 22:46:58 +0700

    Add explain v-model

--- clinic/src/pages/patient/create/CreatePatientPage.vue
+++ clinic/src/pages/patient/create/CreatePatientPage.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
         Create Patient
+
+        <!-- <custom-label-input label="ชื่อ-นามสกุล" v-model="name"/> -->
+        <custom-label-input label="ชื่อ-นามสกุล" @input="handleInput2"/>
+        {{ name }}
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component';
+import { Prop, Component, Vue } from "vue-property-decorator";
+import CustomLabelInputComponent from '@/shared/CustomLabelInputComponent.vue';
 
-@Component
+@Component({components: {'custom-label-input': CustomLabelInputComponent}})
 export default class CreatePatientPage extends Vue {
-  
+  name: string = 'firstname'
+  handleInput2(e) {
+      this.name = e
+  }
 }
 </script>
---
+++ clinic/src/shared/CustomLabelInputComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input type="text" v-model="newInput" @input="handleInput"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class CustomLabelInputComponent extends Vue {
+  @Prop({ default: "label" })
+  label!: string;
+
+  newInput: string = ''
+
+  handleInput (e) {
+      this.$emit('input', this.newInput)
+  }
+}
+</script>
