c32df860be75fbc78750628e15797c7e986f10d3
Suphakrit Nakhabat <suphakrit@odds.team>
2020-02-04 21:07:38 +0700

    Add gender select

--- clinic/src/pages/patient/create/CreatePatientPage.vue
+++ clinic/src/pages/patient/create/CreatePatientPage.vue
@@ -1,18 +1,38 @@
 <template>
-    <div>
-        Create Patient
-
-        <custom-label-input label="ชื่อ-นามสกุล" v-model="name"/>
-        {{ name }}
-    </div>
+  <div>
+    Create Patient
+    <custom-label-input label="ชื่อ-นามสกุล" v-model="name" />
+    <custom-select label="เพศ" :options="genderOptions" v-model="gender" />
+    {{ name }}
+    <br>
+    {{ gender }}
+  </div>
 </template>
 
 <script lang="ts">
 import { Prop, Component, Vue } from "vue-property-decorator";
-import CustomLabelInputComponent from '@/shared/CustomLabelInputComponent.vue';
+import CustomLabelInputComponent from "@/shared/CustomLabelInputComponent.vue";
+import CustomSelectComponent from "@/shared/CustomSelectComponent.vue";
 
-@Component({components: {'custom-label-input': CustomLabelInputComponent}})
+@Component({
+  components: {
+    "custom-label-input": CustomLabelInputComponent,
+    "custom-select": CustomSelectComponent
+  }
+})
 export default class CreatePatientPage extends Vue {
-  name: string = 'firstname'
+  name: string = "firstname";
+  gender: string = "";
+
+  genderOptions: Array<{ text: string; value: string }> = [
+    {
+      text: "male",
+      value: "male"
+    },
+    {
+      text: "female",
+      value: "female"
+    }
+  ];
 }
 </script>
