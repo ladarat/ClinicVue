d3ebab2e75fa2f57c26c1d224ad08e916ee9a7e1
Suphakrit Nakhabat <suphakrit@odds.team>
2020-02-04 22:00:54 +0700

    Create CustomRadioSelectComponent

---
+++ clinic/src/shared/CustomRadioSelectComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <template v-for="(item, index) in options">
+      <input
+        type="radio"
+        name="name"
+        :key="index"
+        :value="item.value"
+        v-model="newInput"
+        @change="handleInput"
+      />
+      {{ item.text }}
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class CustomRadioSelectComponent extends Vue {
+  @Prop({ default: "label" })
+  label!: string;
+
+  @Prop({ default: "" })
+  value!: string;
+
+  @Prop()
+  options!: Array<{ text: string; value: string }>;
+
+  newInput: string = this.value;
+
+  handleInput() {
+    this.$emit("input", this.newInput);
+  }
+}
+</script>
